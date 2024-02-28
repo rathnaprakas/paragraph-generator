70fa135476dbec8b82d37c39012d5cebe1b313c0
rathna1099 <rathnaprakashkdm@gmail.com>
2024-02-28 08:02:35 +0530

    New update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react';
+import data from './data';
+import './index.css'
 
 function App() {
+  let [paras,setParas]=useState(1)
+  let paragraph=data.slice(0,paras)
+  console.log(paragraph)
+  let inputnNoOfParas=useRef()
+  let generateHandler=(event)=>{
+    event.preventDefault();
+    let enteredParas=Number(inputnNoOfParas.current.value);
+    setParas(enteredParas)}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section-center">
+      <h3>TIRED OF BORING?</h3>
+      <div>
+        <form className='lorem-form'>
+          <label htmlFor='noofparas'>Paragraphs:</label>
+          <input type='number' min={1} max={data.length} id='noofparas' ref={inputnNoOfParas} onChange={generateHandler}></input>
+          <button onClick={generateHandler} className='btn'>Generate</button>
+        </form>
+      </div>
+      <div className='lorem-text'>
+        {paragraph.map((i)=><p>{i}</p>)}
+      </div>
     </div>
   );
 }
